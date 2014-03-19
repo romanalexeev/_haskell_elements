07144b2c3aa75fe41307d90d1fd5063b1b783cf9
romanalexeev <alexeev.roman@gmail.com>
2014-03-19 17:07:11 +0400

    distance method with no DistanceType and Attempts typization

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,22 @@ import Transformations
 
 instance Transformable Int
 
-main = putStrLn( show( 
+power_unary_test = putStrLn( show( 
 	let 
 		x = 10 :: Int
 		n = 3 :: Int
 		f = (\i -> i * x)
-	in power 1 n f))
+	in power_unary 1 n f))
 
+distance_test = putStrLn( show(
+	let 
+		x = 2 :: Int
+		y = 3 :: Int
+		f = (\i -> i * x)
+		eq = (\a b -> a == b)
+		a = 10000
+	in distance 1 y f eq a eq
+	))
+
+main = distance_test
 
--- src/Transformations.hs
+++ src/Transformations.hs
@@ -1,8 +1,20 @@
 module Transformations where
 
 type Transform t = t -> t
+type IsEquals t = t -> t -> Bool
 
 class Transformable t where
-	power :: t -> Int -> Transform t -> t
-	power x 0 _ = x
-	power x n f = power (f x) (n - 1) f	
+	
+	power_unary :: t -> Int -> Transform t -> t
+	power_unary x 0 _ = x
+	power_unary x n f = power_unary (f x) (n - 1) f	
+
+	distance :: t -> t -> Transform t -> IsEquals t -> Int -> IsEquals Int -> Int
+	distance x y f eqt a eqi = distance_acc x y f eqt 0 a eqi
+		where
+			distance_acc :: t -> t -> Transform t -> IsEquals t -> Int -> Int -> IsEquals Int -> Int
+			distance_acc x y f eqt n a eqi
+				| eqi n a = -1
+				| eqt x y = n
+				| otherwise = distance_acc (f x) y f eqt  (n + 1) a eqi
+
