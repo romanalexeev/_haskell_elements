2bc33cfd40c5d8f78de5263028148a23a25c31cf
romanalexeev <alexeev.roman@gmail.com>
2014-03-19 15:07:27 +0400

    abstractify Orbits module

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,8 @@
 import Orbits
 
+a = 5 :: Int
+b = 3 :: Int
 
-
-
-
-a = 3 :: Int
-b = 4 :: Int
-
+instance Powerable Int
 main = putStrLn (show (power a b (\i -> i * a)))
 
--- src/Orbits.hs
+++ src/Orbits.hs
@@ -4,8 +4,6 @@ type Transform t = t -> t
 
 class Powerable t where
 	power :: t -> Int -> Transform t -> t
-
-instance Powerable Int where
-	power x 0 _ = 1
 	power x 1 _ = x
-	power x n f = power (f x) (n - 1) f
+	power x n f = power (f x) (n - 1) f	
+
