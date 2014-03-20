218e8572ab162c96ee116fcb4018452a98f83bcb
romanalexeev <alexeev.roman@gmail.com>
2014-03-20 18:59:04 +0400

    using Eq t context instead of IsEqual t functional type

--- src/Main.hs
+++ src/Main.hs
@@ -2,34 +2,34 @@ import Transformations
 
 instance Transformable Int
 
-power_unary_test = putStrLn( show( 
+power_unary_test = putStrLn( "power_unary_test: " ++ show( 
 	let 
 		x = 10 :: Int
 		n = 3 :: Int
 		f = (\i -> i * x)
 	in power_unary 1 n f))
 
-distance_test = putStrLn( show(
+distance_test = putStrLn( "distance_test: " ++ show(
 	let 
 		x = 2 :: Int
-		y = 8 :: Int
+		y = 32 :: Int
 		f = (\i -> i * x)
-		eq = (\a b -> a == b)
 		a = 10000
-	in distance x y f eq a
+	in distance x y f a
 	))
 
-
-collision_point_test = putStrLn( show(
+collision_point_test = putStrLn( "collision_point_test: " ++ show(
 	let 
 		x = 2 :: Int
-		f = (\i -> if i < 10 then i + 1 else 0)
-		eq = (\a b -> a == b)
+		f = (\i -> if i < 10 then i + 1 else 4)
 		p = (\i -> True)
 		a = 10000
 		def = -1
-	in collision_point x f eq a def
+	in collision_point x f a def
 	))
 
-main = collision_point_test
+main = do
+	power_unary_test
+	distance_test
+ 	collision_point_test
 
--- src/Transformations.hs
+++ src/Transformations.hs
@@ -1,8 +1,6 @@
 module Transformations where
 
 type Transform t = t -> t
-type IsEquals t = t -> t -> Bool
---type Predicate t = t -> Bool
 
 class Transformable t where
 	
@@ -10,20 +8,20 @@ class Transformable t where
 	power_unary x 0 _ = x
 	power_unary x n f = power_unary (f x) (n - 1) f	
 
-	distance :: t -> t -> Transform t -> IsEquals t -> Int -> Int
-	distance x y f eq a = distance_acc x y f eq 0 a
+	distance :: (Eq t) => t -> t -> Transform t -> Int -> Int
+	distance x y f a = distance_acc x y f 0 a
 		where
-			distance_acc :: t -> t -> Transform t -> IsEquals t -> Int -> Int -> Int
-			distance_acc x y f eq n a
+			distance_acc :: (Eq t) => t -> t -> Transform t -> Int -> Int -> Int
+			distance_acc x y f n a
 				| n == a = -1
-				| eq x y = n
-				| otherwise = distance_acc (f x) y f eq (n + 1) a
+				| x == y = n
+				| otherwise = distance_acc (f x) y f (n + 1) a
 
-	collision_point :: t -> Transform t -> IsEquals t -> Int -> t -> t
-	collision_point x f eq a def = collision_point_recursive x (f x) f eq 0 a def
+	collision_point :: (Eq t) => t -> Transform t -> Int -> t -> t
+	collision_point x f a def = collision_point_recursive x (f x) f 0 a def
 		where 
-			collision_point_recursive :: t -> t -> Transform t -> IsEquals t -> Int -> Int -> t -> t
-			collision_point_recursive slow fast f eq n a def
+			collision_point_recursive :: (Eq t) => t -> t -> Transform t -> Int -> Int -> t -> t
+			collision_point_recursive slow fast f n a def
 				| n == a = def
-				| eq fast slow = fast
-				| otherwise = collision_point_recursive (f slow) (f (f fast)) f eq (n + 1) a def
+				| fast == slow = fast
+				| otherwise = collision_point_recursive (f slow) (f (f fast)) f (n + 1) a def
